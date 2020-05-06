5688b5392766cbc706a6f931e05e62e7c34d0df0
Shabab Ali <shababsaifi@gmail.com>
2020-05-06 18:32:18 +0530

    Added comment model and comment partial

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_082436) do
+ActiveRecord::Schema.define(version: 2020_05_06_123853) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author_name"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
