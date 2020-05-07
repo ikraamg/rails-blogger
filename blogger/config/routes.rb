05d2fa0610b0d1710cd2c1a6f9f3619b6d5220df
Ikraam <ikraamghoor@gmail.com>
2020-05-07 13:05:36 +0200

    Completed Blog up to I3

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -3,10 +3,10 @@
 Rails.application.routes.draw do
   # resources :articles
 
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
-
-  root to: 'articles#index'
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
