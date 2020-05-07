42f7382225ae1e9782dd84a35953829f63af32ff
Shabab Ali <shababsaifi@gmail.com>
2020-05-07 15:55:31 +0530

    Added tag_list method

--- blogger/db/migrate/20200506082436_create_articles.rb
+++ blogger/db/migrate/20200506082436_create_articles.rb
@@ -5,8 +5,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :body
-
-      t.timestamp
+      t.timestamps
     end
   end
 end
--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_123853) do
+ActiveRecord::Schema.define(version: 2020_05_07_093624) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -24,6 +26,21 @@ ActiveRecord::Schema.define(version: 2020_05_06_123853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_taggings_on_article_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
